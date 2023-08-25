b12e65d515803ffdd470ff64b0324aa0152f6989
Luka Petranović <luka.petranovic@gmail.com>
2023-08-25 18:26:52 +0200

    Added constructor to initialise all start stats to 8 as base point

--- Race.hpp
+++ Race.hpp
@@ -1,9 +1,18 @@
 #pragma once
 class Race
 {
-public:
+private:
 	CString name;
 	int strengthModifier, dexterityModifier, constitutuionModifier,
 		intelligenceModifier, wisdomModifier, charismaModifier;
-};
 
+public:
+	Race()
+		:strengthModifier(8),
+		dexterityModifier(8),
+		constitutuionModifier(8),
+		intelligenceModifier(8),
+		wisdomModifier(8),
+		charismaModifier(8)
+	{}
+};
